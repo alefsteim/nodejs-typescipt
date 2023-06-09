7594d7ea70c50655f7b51b59176b1bcaaadd0b10
Aleksei <alexandrov010790@gmail.com>
2023-06-09 16:05:51 +0300

    Initial setup + working with files

---
+++ main.ts
@@ -0,0 +1,25 @@
+import * as util from 'util';
+import * as fs from 'fs';
+import touch from './utils/touch';
+import cat from './utils/cat';
+
+const command = process.argv[2];
+const path = process.argv[3];
+
+if (command && path) {
+    switch (command) {
+        case 'touch': {
+            touch(path);
+            break;
+        }
+        case 'cat': {
+            cat(path);
+            break;
+        }
+        default: {
+            console.log('Unknown command');
+        }
+    }
+} else {
+    console.log('Command missing');
+}
